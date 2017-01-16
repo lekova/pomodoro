c79684c4faacff58057496bbd073c5c023e1bc9f
lekova <lina.lekova@gmail.com>
2017-01-16 17:03:26 -0500

    add theme color picker, add media query for larger sizes

---
+++ color-switcher.js
@@ -0,0 +1,41 @@
+(function($){
+	$(document).ready(function () {
+		$('.color-picker a').click(function (event) {
+			console.log('hello');
+			event.preventDefault();
+			$(this).addClass('selected-color');
+			var selectedColor = $(this).css('backgroundColor');
+			console.log('selectedColor', rgbToHex(selectedColor));
+			changeThemeColors(selectedColor);
+		});
+
+		//$('.themes').delay(1000).animate({left: '-270px'}, 250);
+
+		$('.panel-toggle').on('click', function() {
+			var $panel = $('.themes');
+			var left = -270;
+
+			if (parseInt($panel.css('left')) == left) {
+				$panel.animate({left: '0px'}, 250);
+			} else if ( parseInt($panel.css('left')) == 0) {
+				$panel.animate({left: '-270'}, 250);
+			}
+		});
+	});
+	
+	function rgbToHex(a){
+		a = a.replace(/[^\d,]/g,"").split(","); 
+		return "#" + ((1 << 24) + ( +a[0] << 16) + (+a[1] << 8) + 
+			+a[2]).toString(16).slice(1)
+	}
+
+	function changeThemeColors(selectedColor) {
+		$('body').css('backgroundColor', selectedColor);
+		$('.themes').css('color', selectedColor);
+		$('.btn').css( 'color', selectedColor);
+		$('.modalbtn').css( 'color', selectedColor);
+		$('.modal').css( 'color', selectedColor);
+		$('.ppc-progress .ppc-progress-fill ').css( 'borderColor', selectedColor);
+		$('.ppc-percents').css( 'backgroundColor', selectedColor);
+	}
+})(jQuery);
